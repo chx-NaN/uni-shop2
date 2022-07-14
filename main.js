35970b5dff253c4a40deb9deff5b9129c862a31b
liqingzhao7 <2655133804@qq.com>
2022-07-14 18:33:39 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 请求拦截器
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+  	title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function () {
+  // do something...
+  uni.hideLoading()
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		
+		<!-- 分类导航区域 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item, i) in navList" :key="i">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +13,33 @@
 	export default {
 		data() {
 			return {
-				
+				//1. 分类导航的数据列表
+				navList: [],
 			};
-		}
+		},
+		onLoad() {
+			// 2. 在 onLoad 中调用获取数据的方法
+			this.getNavList()
+		},
+		methods: {
+			// 3. 在 methods 中定义获取数据的方法
+			async getNavList() {
+				const { data: res } = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message
+			},
+		}
 	}
 </script>
 
 <style lang="scss">
-
+	.nav-list {
+	display: flex;
+	justify-content: space-around;
+	margin: 15px 0;
+	.nav-img {
+	width: 128rpx;
+	height: 140rpx;
+	}
+	}
 </style>
