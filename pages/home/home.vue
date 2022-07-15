e812072d5cd24be1aa3b52d5f4317cd8e973bca4
liqingzhao7 <2655133804@qq.com>
2022-07-15 19:09:53 +0800

    完成了搜索功能的开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
 	<view>
+		<!-- 使用自定义的搜索组件 -->
+		<view class="search-box">
+			<my-search @click="gotoSearch"></my-search>
+		</view>
 		<!-- 轮播图区域 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000"
 		:duration="1000" :circular="true">
@@ -119,7 +123,12 @@
 					})
 				})
 				this.floorList = res.message
-			}
+			},
+			gotoSearch() {
+				uni.navigateTo({
+					url: '/subpkg/search/search'
+				})
+			}
 		}
 	}
 </script>
@@ -153,12 +162,21 @@ swiper {
 }
 
 .right-img-box {
-display: flex;
-flex-wrap: wrap;
-justify-content: space-around;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
 }
 .floor-img-box {
-display: flex;
-padding-left: 10rpx;
+	display: flex;
+	padding-left: 10rpx;
+}
+
+.search-box {
+	// 设置定位效果为“吸顶”
+	position: sticky;
+	// 吸顶的“位置”
+	top: 0;
+	// 提高层级，防止被轮播图覆盖
+	z-index: 999;
 }
 </style>
