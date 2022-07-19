27992bfb8f0931bb01c5f2b986f3da6a602f7924
liqingzhao7 <2655133804@qq.com>
2022-07-19 22:37:45 +0800

    完成了购物车的开发

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import store from '@/store/store.js'
 
 // 导入网络请求的包
 import { $http } from '@escook/request-miniprogram'
@@ -10,17 +11,24 @@ uni.$http = $http
 $http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 // 请求拦截器
-$http.beforeRequest = function (options) {
-  // do somethimg...
+$http.beforeRequest = function(options) {
   uni.showLoading({
-  	title: '数据加载中...'
+    title: '数据加载中...'
   })
 }
 
 // 响应拦截器
-$http.afterRequest = function () {
-  // do something...
+$http.afterRequest = function() {
   uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
 }
 
 Vue.config.productionTip = false
@@ -28,6 +36,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-	...App
+  ...App,
+  store
 })
 app.$mount()
